8e6543675745e0cce36c8c5cc02f9b1fd1044d4e
William <william.j.michaelis@gmail.com>
2025-08-06 23:35:04 -0500

    f
    f

--- features/cards/card_effects/card_effect.gd
+++ features/cards/card_effects/card_effect.gd
@@ -2,12 +2,12 @@ class_name CardEffect
 extends Resource
 
 enum Target {
-    OFFICIAL,
-    OFFICE,
+	OFFICIAL,
+	OFFICE,
 }
 
 @export var target: Target
 
 # Virtual method that subclasses must implement
 func apply_effect(_target_node: Node) -> void:
-    push_error("apply_effect must be implemented by subclass")
+	push_error("apply_effect must be implemented by subclass")
--- features/cards/card_types/card_type.gd
+++ features/cards/card_types/card_type.gd
@@ -2,21 +2,21 @@ class_name CardType
 extends Resource
 
 enum Target {
-    OFFICIAL,
-    OFFICE,
-    GOVERNMENT,
+	OFFICIAL,
+	OFFICE,
+	GOVERNMENT,
 }
 
 @export var name: String
 @export var target: Target
 
 func can_apply_to(target_node: Node) -> bool:
-   match target:
-        Target.OFFICIAL:
-            return target_node is Official
-        Target.OFFICE:
-            return target_node is Office
-        Target.GOVERNMENT:
-            return target_node is Government
-        _:
-            return false
+	match target:
+		Target.OFFICIAL:
+			return target_node is Official
+		Target.OFFICE:
+			return target_node is Office
+		Target.GOVERNMENT:
+			return target_node is Government
+		_:
+			return false
