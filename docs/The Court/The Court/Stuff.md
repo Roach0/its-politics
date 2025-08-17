0b25cd815cb9dde427c2096ce653677a10a2b245
William <william.j.michaelis@gmail.com>
2025-08-16 23:51:40 -0500

    k
    k

---
+++ docs/The Court/The Court/Stuff.md
@@ -0,0 +1,2 @@
+
+- 2 of the same office cannot exist on the board at the same time.  IF an office somehow becomes on the already exists, the original will change to any office that does not yet exist on the board.
--- features/board/board_ui.gd
+++ features/board/board_ui.gd
@@ -3,4 +3,4 @@ extends HBoxContainer
 @onready var game_panel_ui: GamePanelUi = $GamePanelUi
 
 func set_players(players: Array[Player]):
-    game_panel_ui.set_players(players)
+	game_panel_ui.set_players(players)
